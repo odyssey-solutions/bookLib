65470fdad7f48d55a75e6287c62788cb92ccf72d
Blake Turner <blake@comb-communications.co.za>
2022-09-20 22:14:32 +0200

    Initial load

---
+++ EFCoreWebApp/Models/bookModel.cs
@@ -0,0 +1,29 @@
+﻿using BookLibrary;
+
+namespace EFCoreWeb.Models;
+
+public static class bookModelExtentions
+{
+	public static IQueryable<bookModel> toModel(this IQueryable<Book> source)
+	{
+		return source.Select(b => new bookModel
+		{
+			Id = b.Id,
+			Title = b.Title ?? "",
+			YearOfPublication = b.YearOfPublication,
+			Author = (b.Author != null) ? (b.Author.Name ?? "") : ""
+		});
+	}
+}
+
+public class bookModel
+{
+	public int		Id { get; set; }
+	public string	Title { get; set; }
+	public int		YearOfPublication { get; set; }
+	public string 	Author { get; set; }
+
+	public bookModel()
+	{
+	}
+}
